3cb7c4b342af8e2ab408e26b06e32627be4efc3f
George Vlahavas <vlahavas@gmail.com>
2013-08-19 18:58:15 +0300

    Install man page
    Generate the man page with txt2tags and install it when running the
    install.sh script.

--- install.sh
+++ install.sh
@@ -2,8 +2,21 @@
 
 cd $(dirname $0)
 
+if [ -x $( which txt2tags ) ]; then
+        cd man
+        txt2tags xdgmenumaker.t2t
+        cd ..
+else
+        echo "WARNING: txt2tags is not installed. The xdgmenumaker manpage will not be created."
+fi
+
 install -d -m 755 $DESTDIR/usr/bin
 install -d -m 755 $DESTDIR/usr/share/desktop-directories
 install -m 755 src/xdgmenumaker $DESTDIR/usr/bin/
 install -m 644 desktop-directories/* $DESTDIR/usr/share/desktop-directories
+
+if [ -f man/xdgmenumaker.man ]; then
+	install -d -m 755 $DESTDIR/usr/share/man/man1
+	install -m 644 man/xdgmenumaker.man $DESTDIR/usr/share/man/man1/xdgmenumaker.1
+fi
 
